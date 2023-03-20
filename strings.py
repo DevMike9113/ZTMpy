92cfe76a7c4989d5c507258dda79e6d1a3c44b70
DevMike9113 <mike@dawntms.com>
2023-03-20 17:10:04 -0400

    type conversion

--- strings.py
+++ strings.py
@@ -19,3 +19,14 @@ print('hello ' + 'world')
 
 # # will not work
 # print('hello' + 5)
+
+# --------------------------------
+
+# type conversion
+a=str(100)
+b=int(a)
+c=type(b)
+print(c)
+
+# same as above
+print(type(int(str(100))))
