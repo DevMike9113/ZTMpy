521f06dddaddc7ffcf614972670bf0ac23e518f4
DevMike9113 <mike@dawntms.com>
2023-03-20 13:35:28 -0400

    beginning

---
+++ main.py
@@ -0,0 +1,17 @@
+# Fund Data types
+print(2+4) # int
+print(type(2/4)) # float
+
+# to the power of
+print(2 ** 3)
+
+# double slash = rounded down to an integer
+print(6//4)
+
+# modeler = remainder of a divided integer
+print(6%4) # 1
+
+# math functions
+print(round(3.1)) 
+print(abs(-20)) # absolute value - no neg numbers
+
