d04eb174d545551d074fe1f3f71e6a2ef52a5e08
DevMike9113 <mike@dawntms.com>
2023-03-20 14:16:01 -0400

    vars

---
+++ variables.py
@@ -0,0 +1,25 @@
+# variables
+iq = 190
+
+# BEST PRACTICES FOR VARS
+# snake_case
+# Start with lowercase or underscore
+# letters, numbers, underscores
+# case sensitive
+# dont overwrite keywords
+
+user_age = iq/4
+
+z = user_age
+
+# constants = should never be changed
+PI = 3.14
+
+# dont touch these
+
+a,b,c = 1,2,3
+
+print(z)
+print(PI)
+print(iq) 
+print(c)
