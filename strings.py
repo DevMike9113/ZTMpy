5d8b3db7a61233d06f6cc55b8e30a092dac44009
DevMike9113 <mike@dawntms.com>
2023-03-21 11:58:00 -0400

    strings and functions and methods

--- strings.py
+++ strings.py
@@ -31,7 +31,8 @@ print(c)
 # same as above
 print(type(int(str(100))))
 
-# ---------------
+# --------------
+
 # Escape Sequences
 
 # \ = everything after this is a string 
@@ -49,9 +50,54 @@ name = 'Johnny'
 age = 55
 
 # f before string -- new feature in Py3
+# known as an f-string
 print(f'hi 1{name}. You are {age} years old')
 
 # old way
 print('hi 2{}. You are {} years old'.format(name, age))
 print('hi 3{0}. You are {1} years old'.format(name, age))
 print('hi 4{1}. You are {0} years old'.format(name, age))
+print('hi {new_name}. You are {age} years old'.format(new_name="sally", age=100))
+
+# --------------
+
+# string index - bookshelf
+selfish = 'abcdefgh'
+          #01234567 
+print(selfish[4])
+
+# [start:stop]
+print(selfish[3:5])
+
+# [start:stop:stepover]
+print(selfish[1:8:2])
+
+print(selfish[:5])
+print(selfish[2:])
+
+# reverse string
+print(selfish[::-1])
+# the above content is slicing
+
+# ---------------
+
+# immutablility
+# you cant change an existing string
+# you have to assign a completely new thing
+
+# --------------
+
+# Built in functions and methods
+
+# BI function example
+# len = length
+# len counts like humans from 1-9 not like computers from 0-8
+print(len('hello world')) # = 11 characters
+
+# BI Method example
+# .format is a method
+quote = 'to be or not to be'
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+print(quote.replace("be", "me"))
