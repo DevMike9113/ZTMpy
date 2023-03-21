f8606aad46d6a0763244eb87035466d875cc55d9
DevMike9113 <mike@dawntms.com>
2023-03-21 15:59:07 -0400

    password excersise and list

---
+++ Exercises/passwordChecker.py
@@ -0,0 +1,9 @@
+# username = input('Enter Username: ')
+
+# password = input('Enter Password: ')
+
+# password_length = len(password)
+
+# hidden_pass = '*' * password_length
+
+# print(f'{username}, your password {hidden_pass} is {password_length} letters long.')
---
+++ lists.py
@@ -0,0 +1,9 @@
+# lists
+
+li = [1,2,3,4,5]
+li2 = ['a', 'b', 'c']
+li3 = [1,2, 'a', True]
+
+# Data Straucture
+amazon_cart = ["hellocopter", "sun"]
+print(amazon_cart[1])
