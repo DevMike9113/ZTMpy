11a8188a3b8b25f3a45119175f43f4c660e17785
DevMike9113 <mike@dawntms.com>
2023-03-22 11:58:21 -0400

    lists are not immutable

--- lists.py
+++ lists.py
@@ -4,6 +4,40 @@ li = [1,2,3,4,5]
 li2 = ['a', 'b', 'c']
 li3 = [1,2, 'a', True]
 
-# Data Straucture
-amazon_cart = ["hellocopter", "sun"]
-print(amazon_cart[1])
+# Data Structure
+
+# ------------------------------
+
+# List Slicing
+amazon_cart = [
+    "hellocopter", 
+    "sun",
+    'toys',
+    'dinos'
+    ]
+
+# lists ARE NOT immutable - they have the ability to change
+
+amazon_cart[0] = 'space ship'
+# you can change any list item
+
+new_cart = amazon_cart[0:3]
+# BUT when you use list slicing, it creates a new list
+new_cart[1] = 'REX'
+
+newer_cart = amazon_cart[:]
+# IF you want to "copy" a list, you have to SLICE IT with [:]
+newer_cart[3] = 'hot dogs'
+
+print(amazon_cart[1:3])
+print(amazon_cart)
+print(new_cart)
+print(newer_cart)
+
+# ------------------------------
+
+# Matrix
+
+
+
+
