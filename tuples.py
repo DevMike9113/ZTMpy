89fd7bc28907174f64b20d993e8897971e36b41b
DevMike9113 <mike@dawntms.com>
2023-03-31 14:57:53 -0400

    tuples

---
+++ tuples.py
@@ -0,0 +1,30 @@
+# Tuples
+# works like a list, 
+# the difference is that it cannot be modified - immutable
+my_tuple = (1,3,2,3,4,3,5)
+print(my_tuple)
+# less flexible than a list
+# more perfomant than a list
+
+user={
+    (1,2): [4,5,6],
+    'greet': 'hello',
+    'age': 20
+}
+print(user[(1,2)])
+
+# ------------
+
+# Tuples 2
+x,y,z, *other = (1,2,3,4,5)
+print(other)
+
+# count
+# how many of the item is inside the tuple
+print(my_tuple.count(3))
+
+# index
+# stops at the first value that it finds
+print(my_tuple.index(3))
+
+# your also able to use len the same way as you use it with arrays
