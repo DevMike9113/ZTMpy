c9cb4e0d458145e67ccd513ae5e383ef271595a4
DevMike9113 <mike@dawntms.com>
2023-04-04 10:47:37 -0400

    conditionals and logical operators

---
+++ condition.py
@@ -0,0 +1,54 @@
+# is_old = False
+# is_licensed = True
+
+# # if is_old:
+# #     print(
+# #         'you are old enough'
+# #     )
+
+# # elif is_licensed:
+# #     print('you can drive not')
+
+# if is_old and is_licensed:
+#     print(
+#         'you are old enough and have your licensed'
+#     )
+# else:
+#     print('not old enough')
+
+# print('okoko')
+
+# ---------------------
+
+# # ternary operator
+# # conditional expression
+# # condition_if_true if condition else condition_if_else
+
+# is_friend = True
+# can_message = 'message allowed' if is_friend else 'not allowed'
+
+# print(can_message)
+
+# # -----------------
+
+# # Short Circuiting 
+# is_Friend = True
+# is_User = True
+
+# if is_Friend or is_User:
+#     print('best friends forever')
+# # more performant
+# # might be asked in an interview
+
+# ---------------
+
+# Logical opperator
+# >
+# <
+# == # equal, comparing
+# >=
+# <=
+# _ !=
+# not(False)
+
+
