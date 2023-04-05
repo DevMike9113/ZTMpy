4da4529b5bdab7c6ff8b88f6fadee205f92e6f66
DevMike9113 <mike@dawntms.com>
2023-04-05 16:21:05 -0400

    whileLoops and firstGui excersize

---
+++ Exercises/firstGui.py
@@ -0,0 +1,23 @@
+# clean
+# readability
+# predictable
+# DRY = make reusable code
+picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+]
+
+fill = '$'
+empty = ' '
+
+for row in picture:
+    for pixel in row:
+        if (pixel):
+            print(fill, end='')
+        else:
+            print(empty, end='')
+    print('')
---
+++ whileLoops.py
@@ -0,0 +1,49 @@
+# # While Loop
+# # while condition: do something
+# i = 0
+# while 0 < 50:
+#     print('hello')
+# # infinate loop
+# # be careful
+
+# i = 0
+# while i < 50:
+#     print(i)
+#     # i = i + 1
+#     i += 1
+#     # break #else will run only if a break is not present
+# else:
+#     print('done')
+
+## both the for and the while return the same thing
+# my_list = [1,2,3]
+# for item in my_list:
+#     print(item)
+# i = 0
+
+# while i < len(my_list):
+#     print(my_list[i])
+#     i += 1
+
+# while True:
+#     response = input('say something: ')
+#     if (response == 'bye'):
+#         break
+
+my_list = [1,2,3]
+
+# FOR LOOP
+for item in my_list:
+    print(item)
+#    break
+    continue
+
+# # WHILE LOOP
+# i = 0
+# while i < len(my_list):
+#     print(my_list[i])
+#     i += 1
+##    break
+#    # continue  
+
+# 'pass' is used for a placeholder for developing sometimes. it simply passes to the next line and allows the rest of the code to run. Essentially its useless
