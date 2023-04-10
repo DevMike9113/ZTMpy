e144af9620e907ee4539f67038202d128b462f8d
DevMike9113 <mike@dawntms.com>
2023-04-10 12:10:55 -0400

    functions

--- functions.py
+++ functions.py
@@ -9,19 +9,34 @@
 
 # #--------------
 
-# parameters and arguments
+# # parameters and arguments
 
-# params - NAMES of the variables that the function RECEIVES - define
-def say_hello(name='butt', emoji=':0'): #default parameters
-    print (f"hello {name} {emoji}")
+# # params - NAMES of the variables that the function RECEIVES - define
+# def say_hello(name='butt', emoji=':0'): #default parameters
+#     print (f"hello {name} {emoji}")
 
-# args - the VALUES that we PROVIDE to the function - call / invoke
-# positional arguments
-say_hello('Mike', ':)')
+# # args - the VALUES that we PROVIDE to the function - call / invoke
+# # positional arguments
+# say_hello('Mike', ':)')
 
-# ----------
+# # ----------
 
-# key word arguments
-say_hello(name='bibi', emoji=':p')
+# # key word arguments
+# say_hello(name='bibi', emoji=':p')
 
-say_hello() # default parameters
+# say_hello() # default parameters
+
+# -----------------
+
+def sums(n1,n2):
+    def func(r1, r2):
+        return r1 + r2
+    return func(n1,n2)
+    
+# function should do one thing really well
+# should return something
+
+total = sums(10,5)
+print(sums(4,total))
+
+# a return keyword automatically exits a function
