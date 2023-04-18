1ecb92f7fc48c36179fedece2db74b57f0f3b8a5
DevMike9113 <mike@dawntms.com>
2023-04-18 11:25:27 -0400

    imprts

--- sect11-16/importing.py
+++ sect11-16/importing.py
@@ -23,7 +23,45 @@
 # print(random.choice([1, 2, 4, 5, ]))
 #
 # # --------------
+#
+# import sys
+#
+# sys.argv
 
-import sys
+# # -------------
+#
+# from collections import Counter, defaultdict, OrderedDict
+#
+# li = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 7]
+# sentence = 'blah blah blahhhhhh'
+# print(Counter(li))
+# print(Counter(sentence))
+#
+# dictionary = defaultdict(lambda: "doesn't exist", {'a': 1, 'b': 2})
+# print(dictionary['c'])
+#
+# d = OrderedDict()
+# d['a'] = 1
+# d['b'] = 2
+#
+# # in a regular dictionary there is no order
+# d2 = OrderedDict()
+# d2['a'] = 1
+# d2['b'] = 2
+#
+# print(d2 == d)
 
-sys.argv
+# # Recently, the Python has made Dictionaries ordered by default! So unless you need to maintain older version of
+# Python (older than 3.7), you no longer need to use ordered dict, you can just use regular dictionaries!
+
+# # -----------------
+#
+# import datetime
+# from array import array
+#
+# print(datetime.time(5, 5, 5))
+# print(datetime.date.today())
+#
+# # arrays are like lists, but they take up less memory and perform faster
+# arr = array('i', [1, 2, 3])
+# print(arr[1])
