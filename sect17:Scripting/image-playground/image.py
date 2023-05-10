ff4965dd0e03fc86cb031d8fe2d8ca407419eee4
DevMike9113 <mike@dawntms.com>
2023-05-10 13:30:45 -0400

    crop, resize, image processing

--- sect17:Scripting/image-playground/image.py
+++ sect17:Scripting/image-playground/image.py
@@ -1,5 +1,17 @@
 from PIL import Image
 
-img = Image.open('Pokedex/pikachu.jpg')
+img = Image.open('./sect17:Scripting/image-playground/Pokedex/pikachu.jpg')
 
-print(img)
+# fil_img = img.filter(ImageFilter.SMOOTH)
+fil_img = img.convert('L')
+# fil_img.save('grey.png', 'png')  # PNG accepts these different actions
+# print(img)
+# print(img.format)
+# print(img.size)
+# print(img.mode)
+# print(dir(img))
+# fil_img.show()
+# crooked = fil_img.rotate(90)
+# crooked.save("grey.png", 'png')
+resize = fil_img.resize((300, 300))  # resize needs a tuple
+resize.save("grey.png", 'png')
